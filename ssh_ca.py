3b38c01468cbb31e12d7689eeb2d0762b6e12175
Reuben Lifshay <rlifshay@gmail.com>
2017-06-05 02:46:38 -0700

    Reissue expiring certificates and add more configuration options for certificate validity and renewal timing.

--- ssh_ca.py
+++ ssh_ca.py
@@ -1,9 +1,10 @@
 import os
 from os import path
 import logging
+from datetime import datetime, timedelta
 
 try:
-    from sshpki import SshPki
+    import sshpki
 except ImportError:
     pass
 
@@ -11,12 +12,20 @@ log = logging.getLogger(__name__)
 
 def __virtual__():
     try:
-        SshPki
+        sshpki
         return True
     except NameError:
         return False
 
-def ext_pillar(minion_id, pillar, pki_root, ca_privkey, identity_fmt_str='salt_ssh_ca:host:{}', validity_period='4w'):
+def ext_pillar(
+        minion_id,
+        pillar,
+        pki_root,
+        ca_privkey,
+        identity_fmt_str='salt_ssh_ca:host:{}',
+        validity_period='4w',
+        reissue_early_days=7,
+        backdate_days=1):
     log.info("Loading host key certificates for minion '%s'", minion_id)
     pki_root = path.abspath(pki_root)
     log.debug("Using %s as PKI root", pki_root)
@@ -28,7 +37,7 @@ def ext_pillar(minion_id, pillar, pki_root, ca_privkey, identity_fmt_str='salt_s
     if not path.isfile(ca_privkey):
         raise Exception("ca_privkey '{}' must be an existing file".format(ca_privkey))
 
-    pki = SshPki(pki_root, ca_privkey)
+    pki = sshpki.SshPki(pki_root, ca_privkey)
 
     log.debug("Retriving host keys for minion '%s'", minion_id)
     host_keys = __salt__['saltutil.cmd']([minion_id], 'ssh.host_keys', kwarg={'private': False}, expr_form='list')[minion_id]['ret']
@@ -42,8 +51,15 @@ def ext_pillar(minion_id, pillar, pki_root, ca_privkey, identity_fmt_str='salt_s
         cert_path = pki.find_cert(keystr=host_key)
         if cert_path:
             log.debug("Found existing certificate in %s", cert_path)
-        else:
-            cert_path = pki.sign_key(identity_fmt_str.format(minion_id), (minion_id,), '-1d:+' + validity_period, keystr=host_key, host_key=True)
+            cert_data = sshpki.get_cert_info(certfile=cert_path)
+            log.trace("Certificate data: %s", cert_data)
+            cert_expires = datetime.strptime(cert_data['Valid']['to'], '%Y-%m-%dT%H:%M:%S')
+        if not cert_path or cert_expires < datetime.now() + timedelta(days=reissue_early_days):
+            if cert_path:
+                log.info("Certificate for minion '%s' expires soon, reissuing", minion_id)
+            else:
+                log.debug("No matching certificate found. Creating a new one")
+            cert_path = pki.sign_key(identity_fmt_str.format(minion_id), (minion_id,), '-' + str(backdate_days) + 'd:+' + str(validity_period), keystr=host_key, host_key=True)
             log.info("Created new certificate for minion '%s' in %s", minion_id, cert_path)
         with open(cert_path, 'r') as f:
             host_cert = f.read(4096)
